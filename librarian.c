bf2ed596a202704068c14dce8b53ed4d84432531
Hlodr2 <ohi10@outlook.com>
2026-02-20 14:58:47 -0500

    first commit

---
+++ librarian.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "miniaudio/miniaudio.h"
+
+int main(void){
+
+int w = 0;
+
+while ((w <= 0) )
+{
+printf("Audio length in seconds please! (Positive numbers only) \n");
+
+scanf("%d",&w);
+}
+
+printf("%d\n", w);
+
+// look in a folder for audio
+
+
+}
