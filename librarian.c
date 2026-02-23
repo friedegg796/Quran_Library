609e6bc57d2e7c4aa9e824d92a29331fde2ed37a
Hlodr2 <ohi10@outlook.com>
2026-02-23 07:15:05 -0500

    test

--- librarian.c
+++ librarian.c
@@ -1,4 +1,7 @@
 #include <stdio.h>
+#define MA_NO_WAV
+#define MA_NO_MP3
+#define MA_NO_FLAC
 #include "miniaudio/miniaudio.h"
 
 int main(void){
@@ -7,12 +10,18 @@ int w = 0;
 
 while ((w <= 0) )
 {
+
 printf("Audio length in seconds please! (Positive numbers only) \n");
 
-scanf("%d",&w);
+scanf("%u",&w);
+
+if (w <= 1){
+   break;
 }
 
-printf("%d\n", w);
+}
+
+printf("%u\n", w);
 
 // look in a folder for audio
 
