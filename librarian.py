36bea9ff7362d4c5966884d60d432bfaa22d3e88
Hasnain Alahi <ohi10@outlook.com>
2026-03-17 15:42:42 -0400

    Update librarian.py
    give me a moment

---
+++ librarian.py
@@ -0,0 +1,15 @@
+wronginp = True
+
+
+while wronginp == True:
+ 
+
+ audlen = input("Give me the length of audio you want:")
+ 
+ try:
+  audlen = float(audlen)
+  wronginp = False
+ except:
+  print("There was an error, try again.")
+
+print("thank") 
